01e223a60f3d4ccbac50f881fc84b2c97c0bb985
Cristina Murillo <murillocam96@gmail.com>
2018-10-18 16:19:47 -0400

    everything done... bonus could use some work

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,15 @@
 class DogsController < ApplicationController
+    def index 
+        @dogs = Dog.all
+    end
+
+    def show 
+        @dog = Dog.find(params[:id])
+    end
+
+    def sort 
+        d = Dog.all
+        @dogs=d.sort_by{|dog| dog.employees.count}
+        render :index
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
+  get "/dogs/sort", to: "dogs#sort"
+  resources :dogs, only: [:index, :show]
+  resources :employees, only: [:index, :show, :new, :create, :edit, :update]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
